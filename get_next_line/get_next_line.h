34f4cc91fcb6c44482770037918c53d6dfbd8970
Alperen İnce <111581952+aince@users.noreply.github.com>
2023-03-10 18:34:51 +0300

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aince <aince@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 13:50:33 by aince             #+#    #+#             */
+/*   Updated: 2023/01/23 14:40:55 by aince            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+
+#endif
