b703c53860c701df0a97501d0e5b58624e38bb09
Alperen İnce <111581952+aince@users.noreply.github.com>
2023-03-10 18:33:45 +0300

    Add files via upload

---
+++ libft/ft_striteri.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aince <aince@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 15:23:18 by aince             #+#    #+#             */
+/*   Updated: 2022/12/28 21:11:50 by aince            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	if (s)
+	{
+		i = 0;
+		while (s[i])
+		{
+			f(i, &s[i]);
+			i++;
+		}
+	}
+}
+#include <stdio.h>
+void	ft_42(unsigned a, char *b)
+{
+	*b = ft_toupper(*b);
+}
+int	main()
+{
+	char str[] = "alperen";
+	ft_striteri(str, ft_42);
+	printf("%s", str);
+}
+// s stringindeki her karakterin adresine tek tek f fonksiyonu uygulanir.
